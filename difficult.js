3e476f93010f7a1dd8df6fc7dd184e76585262b1
Sneha-4008 <91391103+Sneha-4008@users.noreply.github.com>
2021-09-26 16:11:44 +0530

    Add files via upload

---
+++ difficult.js
@@ -0,0 +1,61 @@
+
+//Questions for moderate level
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which is the casual organism of smallpox",
+    answer: "Variola virus",
+    options: [
+      "Rubella virus",
+      "Variola virus",
+      "Ebola Virus ",
+      "Cytomegalo virus"
+    ]
+  },
+    {
+    numb: 2,
+    question: "In which year Dada Saheb Phalke directed film Raja Harischandra?",
+    answer: "1913",
+    options: [
+      "1911",
+      "1913",
+      "1915",
+      "1917"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The toxic metal associated with the Minamata episode is?",
+    answer: "Mercury",
+    options: [
+      "Mercury",
+      "Arsenic",
+      "Lead",
+      "Cadmium"
+    ]
+  },
+    {
+    numb: 4,
+    question: "ER Diagram in DBMS is a graphical method used to represent?",
+    answer: "Entity class and their relationships",
+    options: [
+      "Primary key and candidate key",
+      "Entity class and their relationships",
+      "Objects and methods with functions",
+      "Entity class and relationship to foreign key only",
+    ]
+  },
+    {
+    numb: 5,
+    question: "Who win The Miss Universe Title 2021?",
+    answer: "Miss Mexico Andrea Meza",
+    options: [
+      "Miss Mexico Andrea Meza",
+      "Miss India Adline Castelino",
+      "Miss Brazil Julia Gama",
+      "None of the above"
+    ]
+},
+
+];
